00d619c9a3f08f30d03ef41e0f6c08737019f62c
yyq252517 <yyq252517@foxmail.com>
2025-06-29 17:31:32 +0800

    change class into interface
    for generated runtime type check

--- interface/ws/base.types.ts
+++ interface/ws/base.types.ts
@@ -1,18 +1,8 @@
-export class Message<type extends string, data = unknown> {
+export interface Message<type extends string, data = unknown> {
     type: type
     data: data
-
-    constructor(type: type, data: data) {
-        this.type = type
-        this.data = data
-    }
 }
 
-export class OcMessage<type extends string, data = unknown, target = string> extends Message<type, data> {
+export interface OcMessage<type extends string, data = unknown, target = string> extends Message<type, data> {
     target: target
-
-    constructor(type: type, data: data, target: target) {
-        super(type, data)
-        this.target = target
-    }
 }
